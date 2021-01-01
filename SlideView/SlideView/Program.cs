68eb3b1941925510927b908e8fd11588708477f8
elfen20 <elfen20@outlook.de>
2021-01-01 15:31:04 +0100

    + commandline arguments

--- SlideView/SlideView/Program.cs
+++ SlideView/SlideView/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Cave.IO;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace SlideView
@@ -16,7 +15,63 @@ namespace SlideView
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FSlideView());
+            var args = Arguments.FromEnvironment();
+            if (args.IsHelpOptionFound() || (args.Parameters.Count == 0))
+            {
+                Console.WriteLine("SlideShow");
+                Console.WriteLine("shows images in a directory or listed in a textfile");
+                Console.WriteLine("usage:");
+                Console.WriteLine("slideshow drive://somedir");
+                Console.WriteLine("slideshow somefile.ext");
+                Console.WriteLine("options: -r randomize images list");
+                Console.WriteLine("options: -s include subdirs (recursive)");
+                return;
+            }
+
+            if (args.IsOptionPresent("f"))
+            {
+
+            }
+            string[] images = null;
+            string param1 = args.Parameters[0];
+            try
+            {
+                if (Directory.Exists(param1))
+                {
+                    SearchOption so = args.IsOptionPresent("s") ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+                    images = Directory.GetFiles(param1, "*.*", so);
+
+
+                }
+                if (File.Exists(param1))
+                {
+                }
+
+                if ((images != null) && (images.Length > 0))
+                {
+                    if (args.IsOptionPresent("r"))
+                    {
+                        int n = images.Length;
+                        Random rnd = new Random();
+                        while (n > 1)
+                        {
+                            n--;
+                            int r = rnd.Next(n + 1);
+                            string tmp = images[r];
+                            images[r] = images[n];
+                            images[n] = tmp;
+                        }
+                    }
+
+                    Application.Run(new FSlideView(images));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+
         }
     }
 }
